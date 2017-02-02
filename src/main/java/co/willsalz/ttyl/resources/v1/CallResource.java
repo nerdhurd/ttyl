84e5ddf7358f0471a602bad22498f95c3f0b118f
Will Salisbury <wjsalisbury@gmail.com>
2017-02-01 23:51:35 -0800

    fix media type?

--- src/main/java/co/willsalz/ttyl/resources/v1/CallResource.java
+++ src/main/java/co/willsalz/ttyl/resources/v1/CallResource.java
@@ -12,13 +12,11 @@ import javax.validation.constraints.NotNull;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("v1/call")
 @Consumes(MediaType.APPLICATION_JSON)
-@Produces(MediaType.APPLICATION_JSON)
 public class CallResource {
 
     private static final Logger logger = LoggerFactory.getLogger(CallResource.class);
