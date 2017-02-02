2e4ce2a0b1b87be773cc7a3150a399d8fe22dcb1
Will Salisbury <wjsalisbury@gmail.com>
2017-02-01 23:58:23 -0800

    hmm

--- src/main/java/co/willsalz/ttyl/resources/v1/CallResource.java
+++ src/main/java/co/willsalz/ttyl/resources/v1/CallResource.java
@@ -4,22 +4,23 @@ import co.willsalz.ttyl.entities.CallRequest;
 import co.willsalz.ttyl.service.PhoneService;
 import com.codahale.metrics.annotation.Timed;
 import com.twilio.rest.api.v2010.account.Call;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.HashMap;
+import java.util.Map;
 
 @Path("v1/call")
 @Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class CallResource {
-
-    private static final Logger logger = LoggerFactory.getLogger(CallResource.class);
 
     private final PhoneService phoneService;
 
@@ -32,7 +33,13 @@ public class CallResource {
     public Response makeCall(@NotNull @Valid final CallRequest callRequest) {
 
         final Call call = phoneService.makeCall(callRequest.getTo());
-        return Response.noContent().build();
+
+        final Map<String, String> response = new HashMap<>();
+        response.put("status", "ok");
+
+        return Response.ok()
+                .entity(Entity.json(response))
+                .build();
 
     }
 
