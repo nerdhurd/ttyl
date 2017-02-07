ecd8214cf1e5d94e6367ee4a6dffe2de8bbdd6e7
Will Salisbury <wjsalisbury@gmail.com>
2017-02-06 21:42:52 -0800

    They couldn't possibly be sending query params in a POST…

--- src/main/java/co/willsalz/ttyl/resources/v1/CallEndedResource.java
+++ src/main/java/co/willsalz/ttyl/resources/v1/CallEndedResource.java
@@ -1,20 +1,18 @@
 package co.willsalz.ttyl.resources.v1;
 
-import co.willsalz.ttyl.entities.TwilioCallRequest;
 import co.willsalz.ttyl.middleware.CsrfFilter;
 import com.codahale.metrics.annotation.Timed;
 import com.twilio.twiml.TwiMLException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
 import javax.annotation.security.PermitAll;
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 @Path("v1/callEnded")
@@ -32,12 +30,9 @@ public class CallEndedResource {
 
     @POST
     @Timed
-    public void callEnded(@NotNull @Valid final TwilioCallRequest req) throws TwiMLException {
+    public void callEnded(@Context HttpServletRequest req) throws TwiMLException {
 
-        logger.info("Callended: {}", req);
-        try (final Jedis redis = pool.getResource()) {
-            redis.del(req.getCallSid());
-        }
+        logger.info("Call Ended: {}", req);
 
     }
 }
--- src/main/java/co/willsalz/ttyl/resources/v1/ConnectCallResource.java
+++ src/main/java/co/willsalz/ttyl/resources/v1/ConnectCallResource.java
@@ -1,6 +1,5 @@
 package co.willsalz.ttyl.resources.v1;
 
-import co.willsalz.ttyl.entities.TwilioCallRequest;
 import co.willsalz.ttyl.middleware.CsrfFilter;
 import com.codahale.metrics.annotation.Timed;
 import com.twilio.twiml.Dial;
@@ -9,22 +8,20 @@ import com.twilio.twiml.Say;
 import com.twilio.twiml.TwiML;
 import com.twilio.twiml.TwiMLException;
 import com.twilio.twiml.VoiceResponse;
+import org.hibernate.validator.constraints.NotEmpty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
 import javax.annotation.security.PermitAll;
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 @Path("v1/connectCall")
-@Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_XML)
 @PermitAll
 @CsrfFilter.CsrfFilterBypass
@@ -39,10 +36,10 @@ public class ConnectCallResource {
 
     @POST
     @Timed
-    public TwiML connectCall(@NotNull @Valid final TwilioCallRequest req) throws TwiMLException {
+    public TwiML connectCall(@NotEmpty @QueryParam("CallSid") final String callSid) throws TwiMLException {
 
         try (final Jedis redis = pool.getResource()) {
-            final String to = redis.get(req.getCallSid());
+            final String to = redis.get(callSid);
 
             // Build our response
             final VoiceResponse voiceResponse = new VoiceResponse.Builder()
