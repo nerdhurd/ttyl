8a912ca3e5af5598b6f07c4d78577040dc4cf94c
Will Salisbury <wjsalisbury@gmail.com>
2017-02-06 21:13:19 -0800

    Fix some shit

--- src/main/java/co/willsalz/ttyl/resources/v1/ConnectCallResource.java
+++ src/main/java/co/willsalz/ttyl/resources/v1/ConnectCallResource.java
@@ -17,12 +17,14 @@ import redis.clients.jedis.JedisPool;
 import javax.annotation.security.PermitAll;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("v1/connectCall")
+@Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_XML)
 @PermitAll
 @CsrfFilter.CsrfFilterBypass
